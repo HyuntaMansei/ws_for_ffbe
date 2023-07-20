f4a8dbb056aba413961c39ea42e34d0c1da66789
HyuntaMansei <jchoi82kor@gmail.com>
2023-07-21 00:18:47 +0900

    some

--- test.py
+++ test.py
@@ -1,4 +1,130 @@
 import streamlit as st
+import mysql.connector
+import pandas as pd
+import numpy as np
+import requests
 
-view = [10,20,30]
-view
+def display_image_link(canvas, image_path=None, caption=None, link_url=None):
+    # Display the image with a clickable link
+    canvas.markdown(f'<div style="text-align: center;"><a href="{link_url}" target="_blank"><img src="{image_path}" alt="{caption}" style="max-width:100%;"></a></div>', unsafe_allow_html=True)
+    # Display the caption
+    canvas.write(caption)
+def show_image(image_urls:list):
+    # # Replace these with the image URLs you want to display
+    # image_urls = [
+    #     "https://img.game8.jp/8378156/07abc8beb1a9891f0d2289099cf00632.png/show",
+    #     "https://img.game8.jp/8229107/bf83dde4914987f13acfc218e5174903.png/show",
+    #     "https://img.game8.jp/8172868/ca520efa4bb343774f08884499ea31c0.png/show",
+    #     # Add more image URLs as needed
+    # ]
+
+    # Replace this with the desired width and height of each image
+    image_width = 100
+    image_height = 150
+
+    # Create a horizontal layout with multiple columns
+    columns = st.columns(len(image_urls))
+
+    # Display each image in a separate column
+    for i, column in enumerate(columns):
+        column.image(image_urls[i], caption=f"Image {i+1}", width=image_width)
+def connect_db():
+    # Connect to the MySQL database
+    db_connection = mysql.connector.connect(
+        host='146.56.43.43',
+        user='ffbemaster',
+        password='aksaksgo1!',
+        database='ffbe')
+
+    return db_connection
+def main():
+    # Replace these with the image URLs you want to display
+    image_urls = [
+        "https://img.game8.jp/8378156/07abc8beb1a9891f0d2289099cf00632.png/show",
+        "https://img.game8.jp/8229107/bf83dde4914987f13acfc218e5174903.png/show",
+        "https://img.game8.jp/8172868/ca520efa4bb343774f08884499ea31c0.png/show",
+        "https://img.game8.jp/8378156/07abc8beb1a9891f0d2289099cf00632.png/show",
+        "https://img.game8.jp/8229107/bf83dde4914987f13acfc218e5174903.png/show",
+        "https://img.game8.jp/8172868/ca520efa4bb343774f08884499ea31c0.png/show",
+        "https://img.game8.jp/8229107/bf83dde4914987f13acfc218e5174903.png/show",
+        "https://img.game8.jp/8172868/ca520efa4bb343774f08884499ea31c0.png/show",
+        # Add more image URLs as needed
+    ]
+
+    # Replace this with the desired width and height of each image
+    image_width = 100
+    image_height = 150
+
+    show_image(image_urls)
+    show_image(image_urls)
+
+    columns = st.columns(8)
+    columns[0].write("I am 1")
+    columns[0].write("I am 1")
+    st.write(888)
+    columns[0].write("I am 1")
+    columns[0].write("I am 1")
+    columns[1].write(2)
+    columns[7].image("https://img.game8.jp/8378156/07abc8beb1a9891f0d2289099cf00632.png/show")
+
+
+    char_name = st.text_input("Character Name:")
+    st.write(char_name)
+
+    db_conn = connect_db()
+    cursor = db_conn.cursor()
+
+    sql = "SELECT char_name, char_img_src, char_main_job FROM char_list"
+    cursor.execute(sql)
+    res = cursor.fetchall()
+    # for r in res:
+    res_data = pd.DataFrame(res, columns=[col[0] for col in cursor.description])
+    st.dataframe(res_data)
+    char_data = res_data.iloc[0]
+
+
+    res_data.items()
+
+    cursor.close()
+    db_conn.close()
+
+    # Image file path
+    image_path = char_data[1]
+
+    # Caption for the image
+    caption = char_data[0]
+
+    # Link URL
+    link_url = "https://www.naver.com"
+    (display_image_link(columns[0], image_path, caption, link_url))
+    (display_image_link(columns[2], image_path, caption, link_url))
+    (display_image_link(columns[4], image_path, caption, link_url))
+
+    # Sample data for the ComboBox options
+    options_map = {
+        'Fruit': ['Apple', 'Banana', 'Orange'],
+        'Vegetable': ['Carrot', 'Broccoli', 'Tomato'],
+        'Animal': ['Dog', 'Cat', 'Elephant'],
+    }
+
+    # Create a ComboBox to select the category
+    category = st.selectbox('Select a category:', ['']+list(options_map.keys()))
+
+    # Check if a category is selected
+    if category:
+        # Get the selected category's options
+        selected_options = options_map.get(category, [])
+
+        # Create a ComboBox to select an option based on the selected category
+        selected_option = st.selectbox('Select an option:', selected_options)
+
+        # Display the selected option
+        if selected_option:
+            st.write(f'You selected: {selected_option}')
+    else:
+        selected_option = st.selectbox('Select an option:', np.random.rand(5))
+        if selected_option:
+            st.write(f'You selected: {selected_option}')
+# Run the Streamlit app
+if __name__ == "__main__":
+    main()
---
+++ ws_for_job_class_vc.py
@@ -0,0 +1,127 @@
+import streamlit as st
+import mysql.connector
+import pandas as pd
+import numpy as np
+import requests
+
+key_number = -1
+
+def main():
+    # You can use HTML inside the Markdown to resize the image.
+    image_url = 'https://img.game8.jp/8029218/4d6fae4744ecaff6f140274a1e448a27.png/show'
+    image_width = 100  # Set your desired width in pixels
+
+    st.markdown(f'<img src="{image_url}" width="{image_width}">', unsafe_allow_html=True)
+
+    st.write("# 무구 비카 by 길드-레오니스")
+    st.write("")
+
+    sql = "select class_alias from class_list"
+    selected_job_class1 = ""
+    selected_job_class2 = ""
+    selected_job_class3 = ""
+
+    selected_job_class1 = get_job_class_name()
+    if selected_job_class1:
+        selected_job_class2 = get_job_class_name()
+        if selected_job_class2:
+            selected_job_class3 = get_job_class_name()
+
+    if selected_job_class1:
+        columns = st.columns(3)
+        columns[0].write(f"무구1: {selected_job_class1}")
+        if selected_job_class2:
+            columns[1].write(f"무구2: {selected_job_class2}")
+            if selected_job_class3:
+                columns[2].write(f"무구3: {selected_job_class3}")
+    else:
+        pass
+    selected_job_classes = [j for j in [selected_job_class1, selected_job_class2, selected_job_class3] if j != ""]
+    if selected_job_classes:
+        vcs = fetch_vcs_for_job_in_brief(selected_job_classes)
+        show_vcs_in_brief(vcs)
+    else:
+        st.write("### 무구를 선택해 주세요.")
+    st.write("# 무구 비전카드 목록 - 자세히(언젠가는...)")
+
+def fetch_vcs_for_job_in_brief(job_classes:list):
+    # 비카이름, 이미지링크, 링크
+    # select * from vc_for_job_list where 검1 = 0 and 검2 = 0 and 검3 = 1 and 총 = 1;
+    sql = "select vc_name, vc_img_src, g8_link from vc_for_job_list where {}".format(' = 1 and '.join(job_classes)) + " = 1"
+    fetched_vcs = fetch_data_from_db(sql)
+    print(fetched_vcs)
+    return fetched_vcs
+def show_vcs_in_brief(vcs:list, col_num:int=4, width=100):
+    columns = st.columns(col_num)
+    cnt = 0
+    print(len(vcs))
+    while cnt < len(vcs):
+        for c in range(col_num):
+            display_image_with_link(*vcs[cnt],width, columns[c])
+            cnt+=1
+            if not cnt < len(vcs):
+                break
+def get_job_class_name():
+    upper_columns = st.columns(3)
+    input_char = upper_columns[0].text_input("캐릭이름:", key=get_key())
+    if input_char:
+        possible_chars = fetch_possible_chars(input_char)
+        selected_char = upper_columns[1].selectbox("캐릭선택", [possible_chars], key=get_key())
+    else:
+        selected_char = upper_columns[1].selectbox("캐릭선택", [""], key=get_key())
+
+    sql = "select class_alias from class_list"
+    class_list = [""] + [d[0] for d in fetch_data_from_db(sql)]
+    if selected_char:
+        fetched_class = fetch_class(selected_char)
+        index = get_index_by_job(fetched_class)
+        selected_job_class = upper_columns[2].selectbox("무구직업", class_list, index=index, key=get_key())
+    else:
+        selected_job_class = upper_columns[2].selectbox("무구직업", class_list, index=0, key=get_key())
+    return selected_job_class
+def get_key():
+    global key_number
+    key_number += 1
+    return key_number
+def fetch_possible_chars(char_name):
+    return [char_name]
+def fetch_class(char_name):
+    return "검1"
+def get_index_by_job(job):
+    return 1
+def display_image_with_link(caption_text=None, image_url=None, hyperlink_url=None, image_width=100, canvas=None):
+    if canvas == None:
+        canvas = st
+        print("Canvas is st")
+
+    centered_image_with_caption = f'''
+        <div style="display: flex; justify-content: center; flex-direction: column; align-items: center;">
+            <a href="{hyperlink_url}" target="_blank" rel="noopener noreferrer">
+                <img src="{image_url}" alt="Image" width="{image_width}">
+            </a>
+            <p style="text-align: center;">{caption_text}</p>
+        </div>
+    '''
+    canvas.markdown(centered_image_with_caption, unsafe_allow_html=True)
+    # # Display the image with a clickable link
+    # canvas.markdown(f'<div style="text-align: center;"><a href="{link_url}" target="_blank"><img src="{image_path} width=50 alt="{caption}" style="max-width:100%;"></a></div>', unsafe_allow_html=True)
+    # # Display the caption
+    # canvas.write(caption)
+
+def connect_db():
+    # Connect to the MySQL database
+    db_connection = mysql.connector.connect(
+        host='146.56.43.43',
+        user='ffbemaster',
+        password='aksaksgo1!',
+        database='ffbe')
+    return db_connection
+def fetch_data_from_db(sql):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    res = [list(d) for d in cursor.fetchall()]
+    return res
+# Run the Streamlit app
+if __name__ == "__main__":
+    main()
