3047be8fe3d6f89a2f3b340e4f0c9da97b1d2d22
HyuntaMansei <jchoi82kor@gmail.com>
2023-07-21 14:28:22 +0900

    some

--- ws_for_job_class_vc.py
+++ ws_for_job_class_vc.py
@@ -5,52 +5,31 @@ import numpy as np
 import requests
 
 key_number = -1
-
 def main():
     divide_screen(4)
     Title_msg = convert_to_center_msg("무구 비카 by 길드-레오니스")
-    Centered_msg = f"""<p style = "font-size: 0.5em; text-align: center;" >{Title_msg}</p>"""
+    Centered_msg = f"""<p style = "font-size: 2em; text-align: center;" >{Title_msg}</p>"""
     st.write(Centered_msg, unsafe_allow_html=True)
 
     sql = "select class_alias from class_list"
-    selected_job_class1 = ""
-    selected_job_class2 = ""
-    selected_job_class3 = ""
 
     columns = st.columns(2)
     selected_job_class1 = get_job_class_name_simple(columns[0])
     selected_job_class2 = get_job_class_name_simple(columns[1])
-    # selected_job_class1 = get_job_class_name()
-    # if selected_job_class1:
-    #     selected_job_class2 = get_job_class_name()
-    #     if selected_job_class2:
-    #         selected_job_class3 = get_job_class_name()
 
     if selected_job_class1:
         columns = st.columns(3)
         columns[0].write(f"무구1: {selected_job_class1}")
         if selected_job_class2:
             columns[1].write(f"무구2: {selected_job_class2}")
-            if selected_job_class3:
-                columns[2].write(f"무구3: {selected_job_class3}")
-    else:
-        pass
-    selected_job_classes = [j for j in [selected_job_class1, selected_job_class2, selected_job_class3] if j != ""]
-    divide_screen(4)
-    # st.write('''<style>
-    # [data-testid="column"] {
-    #     width: calc(25% - 1rem) !important;
-    #     flex: 1 1 calc(25% - 1rem) !important;
-    #     min-width: calc(25% - 1rem) !important;
-    # }
-    # </style>''', unsafe_allow_html=True)
+
+    selected_job_classes = [j for j in [selected_job_class1, selected_job_class2] if j != ""]
     if selected_job_classes:
         vcs = fetch_vcs_for_job_in_brief(selected_job_classes)
         show_vcs_in_brief(vcs,4)
     else:
         st.write("### 무구를 선택해 주세요.")
     st.write("To add later")
-
 def divide_screen(col_num:float):
     r = (1/col_num)*100
     st.write(f'''<style>
@@ -63,18 +42,27 @@ def divide_screen(col_num:float):
 def fetch_vcs_for_job_in_brief(job_classes:list):
     # 비카이름, 이미지링크, 링크
     # select * from vc_for_job_list where 검1 = 0 and 검2 = 0 and 검3 = 1 and 총 = 1;
-    sql = "select vc_name, vc_img_src, g8_link from vc_for_job_list where {}".format(' = 1 and '.join(job_classes)) + " = 1"
+    sql = "select vc_name, vc_trsl_name, vc_jp_name, vc_g8_link, vc_img_src from vc_for_job_list where {}".format(' = 1 and '.join(job_classes)) + " = 1"
     fetched_vcs = fetch_data_from_db(sql)
-    print(fetched_vcs)
+    # print(fetched_vcs)
     return fetched_vcs
 def show_vcs_in_brief(vcs:list, col_num:int=4, width=100):
-    columns = st.columns(col_num)
     cnt = 0
-    print(len(vcs))
     while cnt < len(vcs):
+        columns = st.columns(col_num)
         for c in range(col_num):
-            # display_image_with_link(*vcs[cnt],width, st)
-            display_image_with_link(*vcs[cnt],width, columns[c])
+            # vc: vc_name, vc_trsl_name, vc_jp_name, vc_g8_link, vc_img_src
+            # caption, image, hyperlink
+            vc = vcs[cnt]
+            if vc[0]:
+                vc_name = vc[0]
+            elif vc[1]:
+                vc_name = vc[1]
+            else:
+                vc_name = vc[2]
+            g8_link = vc[3]
+            image_src = vc[4]
+            display_image_with_link(vc_name, g8_link, image_src, width, columns[c])
             cnt+=1
             if not cnt < len(vcs):
                 break
@@ -113,12 +101,9 @@ def fetch_class(char_name):
     return "검1"
 def get_index_by_job(job):
     return 1
-def display_image_with_link(caption_text=None, image_url=None, hyperlink_url=None, image_width=50, canvas=None):
+def display_image_with_link(caption_text=None, hyperlink_url=None, image_url=None, image_width=50, canvas=None):
     if canvas == None:
         canvas = st
-    # max_char_len = 60
-    # adding_space = '&nbsp;'*(int)((max_char_len-1.5*len(caption_text))/2)
-    # caption_text = caption_text+adding_space
     centered_image_with_caption = f'''
         <div style="display: flex; justify-content: center; flex-direction: column; align-items: center;">
             <a href="{hyperlink_url}" target="_blank" rel="noopener noreferrer">
@@ -130,14 +115,11 @@ def display_image_with_link(caption_text=None, image_url=None, hyperlink_url=Non
         </div>
     '''
     canvas.markdown(centered_image_with_caption, unsafe_allow_html=True)
-    # if (len(caption_text) - 0.5*caption_text.count(" ")) < 8.5:
-    #     canvas.write("")
 def convert_to_center_msg(msg:str):
     return f'''
         <div style="display: flex; justify-content: center;">
             <h3>{msg}</h3>
-        </div>
-    '''
+        </div>'''
 def connect_db():
     # Connect to the MySQL database
     db_connection = mysql.connector.connect(
@@ -151,6 +133,8 @@ def fetch_data_from_db(sql):
     cursor = conn.cursor()
     cursor.execute(sql)
     res = [list(d) for d in cursor.fetchall()]
+    cursor.close()
+    conn.close()
     return res
 # Run the Streamlit app
 if __name__ == "__main__":
